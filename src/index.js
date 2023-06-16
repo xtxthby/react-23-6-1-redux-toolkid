5007753cc0c3a0b58ad09966f2f2e4a0f7c936c0
Yurii <116266510+xtxthby@users.noreply.github.com>
2023-06-16 16:26:58 +0300

    add files

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { App } from 'components/App';
+import { store, persistor } from './redux/store';
+
+const theme = {};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
